13c5aa4534872f6c78e328834af38c6d8302f70b
Chris <chris@cd-developer.com>
2026-02-04 00:25:57 -0500

    Add project files.

---
+++ MyGolfAPI/Data/MyGolfDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyGolfAPI.Entities;
+
+namespace MyGolfAPI.Data
+{
+    public class MyGolfDbContext : DbContext
+    {
+        public MyGolfDbContext(DbContextOptions<MyGolfDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+    }
+}
---
+++ MyGolfAPI/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MyGolfAPI.Entities
+{
+    public class User
+    {      
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = string.Empty;
+
+
+    }
+}
